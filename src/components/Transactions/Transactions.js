c199058ca232d41db4bd6e2e6440c93697a15fcb
DimitarChristoff <christoff@gmail.com>
2016-11-09 13:51:21 +0000

    font-awesome local and fixes to info on unknown payee

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -35,14 +35,13 @@ export default class Transactions extends React.Component {
             </tr>
           </thead>
           <tbody>
-            { next24h.map(toTransaction) }
+            { next24h.map(this.toTransaction, this) }
           </tbody>
         </table>
       </div>
       <div className="box">
         <h2 className="title">Last 24 hours</h2>
         <h3 className="subtitle">Already spent: {money(last24h.reduce(toTotal, 0))}</h3>
-
         <table className="table is-narrow">
           <thead>
           <tr>
@@ -64,7 +63,7 @@ export default class Transactions extends React.Component {
           </tr>
           </thead>
           <tbody>
-            { last24h.map(toTransaction) }
+            { last24h.map(this.toTransaction, this) }
           </tbody>
         </table>
 
@@ -72,41 +71,55 @@ export default class Transactions extends React.Component {
     </div>
 
   }
+
+  handleExpand(item){
+    item.expanded = item.expanded ? false : true;
+    this.forceUpdate()
+  }
+
+  toTransaction(t){
+    const className = classNames({
+      unfamiliar: t.unfamiliar,
+      debit: t.transactionAmount < 0,
+      credit: t.transactionAmount > 0,
+      'info animated fadeInRight': t.expanded
+    })
+
+    if (t.expanded){
+      return <tr className={className}>
+        <td colSpan="4" className="description">
+          New payee "{t.transactionDescription}"
+        </td>
+        <td className="cta">
+          <button title="Dismiss" onClick={() => this.handleExpand(t)} className="button is-small"><i className="fa fa-times"/></button>
+        </td>
+      </tr>
+    }
+
+    return <tr key={ `t-${ t.id }` } className={className}>
+      <td className="description" title={t.transactionDescription}>
+        {t.transactionDescription}
+      </td>
+      <td className="date">
+        <div className="is-hidden-mobile">
+          {moment(t.transactionDateTime).format('DD/MM/YYYY')}
+        </div>
+        <div className="is-hidden-tablet">
+          {moment(t.transactionDateTime).format('DD/MM')}
+        </div>
+      </td>
+      <td className="has-text-right transaction transaction-amount">
+        {money(t.transactionAmount)}
+      </td>
+      <td className="has-text-right transaction">
+        {money(t.accountBalance)}
+      </td>
+      <td className="cta">
+        <button onClick={() => this.handleExpand(t)} className="button is-small"><i className="fa fa-eye"/></button>
+      </td>
+    </tr>
+  }
 }
-
-function toTransaction(t){
-  const className = classNames({
-    unfamiliar: t.unfamiliar,
-    debit: t.transactionAmount < 0,
-    credit: t.transactionAmount > 0
-  })
-
-  return <tr key={ `t-${ t.id }` } className={className}>
-    <td className="description" title={t.transactionDescription}>
-      {t.transactionDescription}
-    </td>
-    <td className="date">
-      <div className="is-hidden-mobile">
-        {moment(t.transactionDateTime).format('DD/MM/YYYY')}
-      </div>
-      <div className="is-hidden-tablet">
-        {moment(t.transactionDateTime).format('DD/MM')}
-      </div>
-    </td>
-    <td className="has-text-right transaction transaction-amount">
-      {money(t.transactionAmount)}
-    </td>
-    <td className="has-text-right transaction">
-      {money(t.accountBalance)}
-    </td>
-    <td className="cta">
-      <button className="button is-small"><i className="fa fa-eye"/></button>
-    </td>
-  </tr>
-}
-
-const toTransactionField = t => field =>
-  <p key={ `t-${ t.id }-${ field }`} >{ `${ field }: ${ t[field] }` }</p>
 
 function toTotal(total, t) {
   return Math.abs(total + t.transactionAmount)
