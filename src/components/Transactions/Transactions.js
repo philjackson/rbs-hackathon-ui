f16dd73c3726bf2220b5fdd72e64fdb3ece9dc8f
Leon Coto <leoncoto@gmail.com>
2016-11-09 10:39:02 +0000

    Add period balances

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -9,12 +9,14 @@ export default class Transactions extends React.Component {
     const { last24h, next24h, accountBalance } = this.props.store
     return <div style={{ padding: 10 }}>
       <h2 style={{ marginBottom: 5 }}>Next 24 hours</h2>
+      <p>To be spent: { next24h.reduce(toTotal, 0) }</p>
       <ul
         style={{ height: 200, overflow: 'scroll' }}>
         { next24h.map(toTransaction) }
       </ul>
 
       <h2 style={{ marginBottom: 5 }}>Last 24 hours</h2>
+      <p>Already spent: { last24h.reduce(toTotal, 0) }</p>
       <ul
         style={{ height: 200, overflow: 'scroll', marginBottom: 10 }}>
         { last24h.map(toTransaction) }
@@ -40,5 +42,9 @@ const toTransactionField = t => field =>
 
 function unfamiliar() {
   return <i className="fa fa-exclamation-triangle unfamiliar"/>
+}
+
+function toTotal(total, t) {
+  return total + t.transactionAmount
 }
 
